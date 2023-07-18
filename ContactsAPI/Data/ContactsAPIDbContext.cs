569c90354b1b02d58694c264847090bc225cac7a
pratibhapatel93 <114645501+pratibhapatel93@users.noreply.github.com>
2023-07-18 10:58:42 +0530

    Add files via upload

---
+++ ContactsAPI/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ContactsAPI.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+namespace ContactsAPI.Data
+
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options) 
+        { 
+        }
+        
+        public DbSet<Contact> Contacts { get; set; }
+
+    
+        
+
+    }
+
+
+
+
+}
